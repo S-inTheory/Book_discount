c6aad76c03e141126bfd5abf3bac6d2ff79bfecd
Alexander Yarchenko <livsi48@gmail.com>
2020-03-14 13:40:53 +0300

    homework 4

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,17 +1,21 @@
-from flask import Flask, render_template, flash, redirect, url_for
-from flask_login import LoginManager, login_required, current_user, login_user, logout_user
+from flask import Flask, render_template
+from flask_login import LoginManager, login_required
+from flask_migrate import Migrate
 
-from webapp.model import db, User
-from webapp.forms import LoginForm
+from webapp.user.models import db, User
+from webapp.user.views import blueprint as user_blueprint
+
 
 def create_app():
     app = Flask(__name__)
     app.config.from_pyfile('config.py')
     db.init_app(app)
+    migrate = Migrate(app, db)
 
     login_manager = LoginManager()
     login_manager.init_app(app)
-    login_manager.login_view = 'login'
+    login_manager.login_view = 'user.login'
+    app.register_blueprint(user_blueprint)
 
     @login_manager.user_loader
     def load_user(user_id):
@@ -20,34 +24,7 @@ def create_app():
     @app.route('/')
     def index():
         page_title = 'Главная страница'
-        return render_template('index.html', title=page_title)
-
-
-
-    @app.route('/login')
-    def login():
-        if current_user.is_authenticated:
-            return redirect(url_for('index'))
-        login_form = LoginForm()
-        return render_template('login.html', title='Sign In', form=login_form)
-
-    @app.route('/logout')
-    def logout():
-        logout_user()
-        flash('Вы успешно разлогинились')
-        return redirect(url_for('index'))
-
-    @app.route('/process_login', methods=['POST'])
-    def process_login():
-        form = LoginForm()
-        if form.validate_on_submit():
-            user = User.query.filter_by(username=form.username.data).first()
-            if user and user.check_password(form.password.data):
-                login_user(user)
-            flash('Вы вошли на сайт')
-            return redirect(url_for('index'))
-        flash('Неправильное имя пользователя или пароль')
-        return redirect(url_for('login'))
+        return render_template('books/index.html', title=page_title)
 
     @app.route('/main')
     @login_required
